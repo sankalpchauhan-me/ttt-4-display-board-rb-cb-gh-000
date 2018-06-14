ce0678565dddc594107148c3303e4795260d9e60
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-06-14 06:20:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts " #{board[6]} | #{board[6]} | #{board[6]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[6]} | #{board[6]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
